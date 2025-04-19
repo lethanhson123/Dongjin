f5ea01d0c0e03ed018e963c3c396c5437ec7f3d2
lethanhson1234 <tinhnghesohu@gmail.com>
2025-04-19 21:04:51 +0700

    20250419

--- MES/src/app/shared/MES/tspart_ecn.service.ts
+++ MES/src/app/shared/MES/tspart_ecn.service.ts
@@ -1,5 +1,6 @@
 ﻿import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { MatTableDataSource } from '@angular/material/table';
 import { environment } from 'src/environments/environment';
 import { tspart_ecn } from './tspart_ecn.model';
 import { BaseService } from './Base.service';
@@ -9,9 +10,14 @@ import { BaseService } from './Base.service';
 export class tspart_ecnService extends BaseService {
     DisplayColumns001: string[] = ['No', 'PARTECN_IDX', 'PART_IDX', 'PART_ENCNO', 'PART_ECN_DATE', 'DWG_NO', 'DWG_FILE_GRP', 'DWG_FILE_EXPOR', 'PART_ECN_USENY', 'CREATE_DTM', 'CREATE_USER', 'UPDATE_DTM', 'UPDATE_USER', 'Save'];
     DisplayColumns002: string[] = ['PART_NO', 'PART_NAME', 'PART_ECN_DATE', 'PART_ENCNO', 'DWG_NO', 'FVWR', 'DWG_FILE_GRP', 'ADDFILE', 'CREATE_DTM', 'CREATE_USER'];
+    DisplayColumns003: string[] = ['PART_NO', 'PART_NAME', 'PART_ENCNO', 'PART_ECN_DATE', 'DWG_NO', 'DWG_FILE_GRP', 'DWG_FILE_EXPOR', 'PART_ECN_USENY', 'CREATE_DTM', 'CREATE_USER', 'UPDATE_DTM'];
+
+    DataSourceTabPage3: MatTableDataSource<any>;    
+
     List: tspart_ecn[] | undefined;
     ListFilter: tspart_ecn[] | undefined;
     FormData!: tspart_ecn;
+    ListTabPage3: tspart_ecn[] | undefined;
     constructor(public httpClient: HttpClient) {
         super(httpClient);
         this.Controller = "v1/tspart_ecn";
